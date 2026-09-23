7089901f65cb1274e4343dc7282994537668e972
Anjali-Nayak-prog <anjaliknayak143@gmail.com>
2026-09-23 17:58:18 +0530

    Configure About and Contact pages

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,26 +1,47 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+URL configuration for djangoproj project.
+
+The `urlpatterns` list routes URLs to views.
+"""
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
+    # Django administration
     path('admin/', admin.site.urls),
+
+    # Django application APIs and views
     path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    # Home page
+    path(
+        '',
+        TemplateView.as_view(template_name='Home.html'),
+        name='home'
+    ),
+
+    # About Us page
+    path(
+        'about',
+        TemplateView.as_view(template_name='About.html'),
+        name='about'
+    ),
+
+    # Contact Us page
+    path(
+        'contact',
+        TemplateView.as_view(template_name='Contact.html'),
+        name='contact'
+    ),
+]
+
+# Serve static files during development
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
